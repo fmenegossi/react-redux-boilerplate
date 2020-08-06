b5186ffd5b22842463964bbcb2a32b557ac15490
Felipe Menegossi <felipe.menegossi@gmail.com>
2020-08-06 22:41:45 +0200

    Wrap with connected-react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import store from './redux/store'
+import store, { history } from './redux/store'
+import { ConnectedRouter } from 'connected-react-router'
 import App from './containers/App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render((
-  <Provider store={store}>
-    <App />
-  </Provider>
-),
-document.getElementById('root')
+ReactDOM.render(
+  (
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </Provider>
+  ), document.getElementById('root')
 )
 
 // If you want your app to work offline and load faster, you can change
